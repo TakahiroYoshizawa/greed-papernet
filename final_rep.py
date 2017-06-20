a5cef9bef6081e77ce075a8f180864e9852a2120
TakahiroYoshizawa <takataku.shako.11326@gmail.com>
2017-06-20 22:52:08 +0900

    New

---
+++ final_rep.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+import pandas   as pd
+import numpy    as np
+import seaborn  as sns
+import networkx as nx
+import os
+import matplotlib.pyplot  as plt
+
+citations = pd.read_csv('citations.csv', encoding='utf-8', index_col=0)
+Graph = nx.DiGraph()
+Graph.add_nodes_from(list(citations.index))
+for i in list(citations.index):
+    x = list(citations.ix[str(i),:])
+    x = [j for j in x if str(j) != 'nan']
+    edgelist = [(str(i),k) for k in x]
+    Graph.add_edges_from(edgelist)
+
+pos = nx.spring_layout(Graph)
+nx.draw_networkx(Graph, pos, fontsize=0, nodesize=50, with_labels=True)
+plt.show()
